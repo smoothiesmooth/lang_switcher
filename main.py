18394832ffd650addb8a2125aa41001db1aff010
Gregor <38989835+BabyMorti@users.noreply.github.com>
2020-03-15 15:46:42 +0300

    added design

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from db import rus, eng
-action = input('Eng or Rus:\n')
-if action == 'Rus':
-	s = input()
+import tkinter as tk
+
+def Rus():
+	s = entry.get()
 	out = ''
 	input = []
 	output = []
@@ -11,9 +12,11 @@ if action == 'Rus':
 		output.append(rus[c])
 	for x in output:
 		out+=x
-	print(out)
-if action == 'Eng':
-	s = input()
+	label = tk.Label(text=f'{out}')
+	label.pack(pady=60)
+
+def Eng():
+	s = entry.get()
 	out = ''
 	input = []
 	output = []
@@ -23,4 +26,34 @@ if action == 'Eng':
 		output.append(eng[c])
 	for x in output:
 		out+=x
-	print(out)
+	label = tk.Label(text=f'{out}')
+	label.pack(pady=60)
+
+win = tk.Tk()
+win.title("Lang Switcher")
+win.geometry("700x400")
+win.iconbitmap(r'icon.ico')
+button2 = tk.Button(
+    text="Rus",
+    width=10,
+    height=2,
+    bg="grey",
+    fg="white",
+	command=Rus,
+)
+button1 = tk.Button(
+    text="Eng",
+    width=10,
+    height=2,
+    bg="grey",
+    fg="white",
+	command=Eng,
+)
+entry = tk.Entry(fg="black", width=50)
+entry.pack()
+inf = tk.Label(text="Beta 0.0.1, developed by Gregor Zhilich")
+button1.place(x=400,y=30)
+button2.place(x=220,y=30)
+inf.place(x=0,y=380)
+
+win.mainloop()
